825411004a51aea9cab125f44142ab1cefd8d8ba
Scott McKean <scott.mckean@ucalgary.ca>
2020-10-16 13:58:27 -0600

    Finish experimental data prep

---
+++ R/functions.R
@@ -0,0 +1,21 @@
+
+#' Make a model of vp ~ confining + deviatoric stress for mapping
+#' @param df
+#' @return a linear model of vp
+#' @export
+fit_vp_model <- function(df){
+  lm(vp ~ confining_pressure + deviatoric_stress, data = df)
+}
+
+#' Make a model of vs ~ confining + deviatoric stress for mapping
+#' @param df
+#' @return a linear model of vs
+#' @export
+fit_vs_model <- function(df){
+  lm(vs ~ confining_pressure + deviatoric_stress, data = df)
+}
+
+
+
+
+
